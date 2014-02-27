9bac20e39833f189889e0d5ccd7e2337525e7338
Jason <jason.h42@gmail.com>
2014-02-26 16:41:54 -0800

    Removing unused code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @units = Unit.includes(:UnitPhotos, :City, :State).first(1000)
     @units = Unit.all.page(params[:page]).per(10)
   end
 
