ec257fb2b3f1983e135c9a9036d19062e5b732ff
Jason <jason.h42@gmail.com>
2014-03-05 19:51:01 -0800

    Adding paging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if params[:query].present?
-      @units = Unit.search(params[:query], page: params[:page], fields: [:ad_headline], misspellings: false)
+      @units = Unit.search(params[:query], page: params[:page], per_page: 10, fields: [:ad_headline], misspellings: false)
     else
       @units = Unit.all.page params[:page]
     end
