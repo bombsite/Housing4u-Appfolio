86c83e672ce9e728c79f5fc2ab5885dbb7fbe488
Jason <jason.h42@gmail.com>
2014-03-05 19:43:54 -0800

    Fixing the Levenshtein distance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if params[:query].present?
-      @units = Unit.search(params[:query], page: params[:page], fields: [:ad_headline])
+      @units = Unit.search(params[:query], page: params[:page], fields: [:ad_headline], misspellings: false)
     else
       @units = Unit.all.page params[:page]
     end
