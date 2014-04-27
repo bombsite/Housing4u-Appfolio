e2051ff7bad7e13ed24cac8d7153bb3952d084a6
Site Mao <site.mao1992@gmail.com>
2014-04-27 16:56:41 -0700

    faceboook auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408000008) do
+ActiveRecord::Schema.define(version: 20140427224724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 20140408000008) do
     t.datetime "updated_at"
     t.string   "phone_number"
     t.string   "company_name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
