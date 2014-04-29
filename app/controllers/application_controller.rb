3f8e25992b85ba02d0aa2be261622e288892eb58
Alvin Tan <alvin9845@gmail.com>
2014-04-29 00:21:54 -0700

    view profile page, edit profile page, new sign up page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :phone_number << :company_name
+    devise_parameter_sanitizer.for(:account_update) << :phone_number << :company_name
+  end
 end
