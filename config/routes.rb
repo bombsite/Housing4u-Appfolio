a8b6fe5fcfa353a533c63f0c17ee8eced8ac7c1b
Alvin Tan <alvin9845@gmail.com>
2014-04-30 03:03:34 -0700

    more profile editing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Untitled::Application.routes.draw do
-  devise_for :users,  :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users,  :controllers => { :omniauth_callbacks => "omniauth_callbacks", :registrations => "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
