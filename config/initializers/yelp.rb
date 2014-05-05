efe15908ecfb619aa702f128dab61ff8db445ccb
Austin Alameda <austinalameda@umail.ucsb.edu>
2014-05-04 22:56:42 -0700

    Added yelp keys Fixed city to only show after state selected in Search Changed text 'advanced search' to 'search' and removed accordion Added buttons for map-view sidebar # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   Gemfile #	modified:   Gemfile.lock #	modified:   app/assets/javascripts/application.js #	modified:   app/views/units/show.html.erb #	modified:   db/schema.rb #

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,5 @@
+Yelp.configure(:yws_id          => 'YBCCDdP_SZupgYya9yys8w',
+               :consumer_key    => '2tFZPoLDwsh27ZinSnN1Sw',
+               :consumer_secret => 'uyXTGwXEKP-zu83NS5Xs7os1WlI',
+               :token           => '1SnuvokfE2hA3TpO5nbW25z7kMsqrMtI',
+               :token_secret    => 'sFm3D-rnQmxazf344WIfGnxNUVM')
