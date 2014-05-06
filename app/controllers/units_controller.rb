ee7d555ec3b49d05d4fac46e3b4b478bac320c55
Austin <austinalameda@umail.ucsb.edu>
2014-05-06 16:12:53 -0700

    et defailt map zoom # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   Gemfile #	modified:   Gemfile.lock #	modified:   app/views/units/show.html.erb # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	vendor/bundle/

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -18,14 +18,6 @@ class UnitsController < ApplicationController
 
   def show
     @unit = Unit.find(params[:id])
-    client = Yelp::Client.new
-
-
-    request = GeoPoint.new(
-        :latitude => 37.782093,
-        :longitude => -122.483230)
-    response = client.search(request)
-    puts response
   end
 
   def search
